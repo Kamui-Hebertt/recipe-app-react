83eb28c61b2b77196f105ef6a0a20a489eb81669
Thiago <thidurante@hotmail.com>
2022-11-29 16:44:18 -0300

    feat: Adds providers/BrowserRouter

---
+++ src/context/FoodProvider.jsx
@@ -0,0 +1,13 @@
+import FoodContext from './FoodContext';
+
+export default function FoodProvider({ children }) {
+  return (
+    <FoodContext.Provider value={ }>
+      <div>
+        { children }
+      </div>
+    </FoodContext.Provider>
+  );
+}
+
+FoodProvider.propTypes = {}.isRequired;
