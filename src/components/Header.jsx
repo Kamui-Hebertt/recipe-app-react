892870566a5d69a66128c8f9d3026d7f26c6970a
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2022-12-09 20:31:37 -0300

    feat: header: style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,34 @@ import PropTypes from 'prop-types';
 import { useContext, React, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import FoodContext from '../context/FoodContext';
-import profile from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
+import profile from '../images/profile.png';
+import searchIcon from '../images/search.png';
 import SearchBar from './SearchBar';
 import './Header.css';
 import mealIcon from '../images/mealIcon.svg';
+import Recipe from '../images/recipeIcon.png';
+import app from '../images/App.png';
+import recipes from '../images/Recipes.png';
 
 function Header(props) {
   const history = useHistory();
   const { searchValue, setSearchValue } = useContext(FoodContext);
   const { search, pageName } = props;
   const [searchBar, setSearchBar] = useState(false);
+
   return (
     <section className="all-btn">
-      <div className="all">
 
+      <div className="all">
+        <img src={ Recipe } id="iconR" alt="recipe icon" />
+        <div className="title">
+          <img src={ app } id="app" alt="app" />
+          <img src={ recipes } id="re" alt="recipe" />
+        </div>
         {search ? (
           <button
+            className="searchBtn"
             type="button"
-            src={ searchIcon }
             data-testid="search-top-btn"
             onClick={ () => (searchBar === false
               ? setSearchBar(true) : setSearchBar(false)) }
@@ -34,7 +43,7 @@ function Header(props) {
           name="profile"
           id="profile"
           data-testid="profile-top-btn"
-          src={ profile }
+          className="profileBtn"
           alt="profile"
           onClick={ () => history.push('/profile') }
           type="button"
